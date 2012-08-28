b122bfbb3966b5da7a740d95196a8d8bf8b6bb6d
Kibet Yegon <kibyegn@gmail.com>
2012-08-28 23:43:02 +0300

    added spying/showing of individual pages on clicks

--- bacon_seek.rb
+++ bacon_seek.rb
@@ -4,39 +4,49 @@ require 'json'
 require 'koala'
 
 class BaconSeek < Sinatra::Base
-  set :static, true
-  set :public, 'public'
-  set :views,  'views'
   set :show_exceptions, true
+  set :logging, true
+  set :dump_errors, true
+  set :raise_errors, true
 
   get '/' do
-    erb :index
+    show "index"
   end
 
   get '/search' do
-    erb :index
+    show "index"
   end
 
   post '/search' do
-    begin
-      @graph = Koala::Facebook::API.new
-      @results = nil
-      if params[:query] =~ /\[/ #contains Object type filter
-        object_type, *search_query = params[:query].split(/\]/)
-        @results = @graph.search(search_query, {:type => "#{object_type[1..-1]}"})
-      else
-        @results = @graph.search(params[:query])
-      end
-      @results.each_with_index do |item, index|
-        @results[index]["likes"] = @graph.get_object(item["id"])["likes"] #adding likes to hash
-      end
-      erb :search
-    rescue => error
-      erb :n00b
+    graph unless @graph
+    @results = nil
+    if params[:query] =~ /\[/ #contains Object type filter
+      object_type, *search_query = params[:query].split(/\]/)
+      @results = @graph.search(search_query, {:type => "#{object_type[1..-1]}"})
+    else
+      @results = @graph.search(params[:query])
     end
+    @results.each_with_index do |item, index|
+      @results[index]["likes"] = @graph.get_object(item["id"])["likes"]
+    end
+    show "search"
   end
 
-  get '/n00b' do
-    erb :n00b
+  get "/spy/:id" do
+    graph unless @graph
+    @fb_object = @graph.get_object(params[:id])
+    show "spy"
+  end
+
+  def graph
+    @graph = Koala::Facebook::API.new
+  end
+
+  def show(page, layout = true)
+    begin
+      erb page.to_sym, {:layout => layout}
+    rescue
+      erb :error, {:layout => false}, :error => "You broke it!"
+    end
   end
 end
