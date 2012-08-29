2ef9526d7a45926c94a995233675b58a44fcf95c
Kibet Yegon <kibyegn@gmail.com>
2012-08-29 18:34:34 +0300

    added images to spy page

--- bacon_seek.rb
+++ bacon_seek.rb
@@ -6,7 +6,6 @@ require 'koala'
 class BaconSeek < Sinatra::Base
   set :show_exceptions, true
   set :logging, true
-  set :dump_errors, true
   set :raise_errors, true
 
   get '/' do
@@ -34,7 +33,9 @@ class BaconSeek < Sinatra::Base
 
   get "/spy/:id" do
     graph unless @graph
-    @fb_object = @graph.get_object(params[:id])
+    item = @graph.get_object(params[:id])
+    @links = get_links(item)
+    @item = strip_redundants(item)
     show "spy"
   end
 
@@ -45,8 +46,30 @@ class BaconSeek < Sinatra::Base
   def show(page, layout = true)
     begin
       erb page.to_sym, {:layout => layout}
-    rescue
-      erb :error, {:layout => false}, :error => "You broke it!"
+    rescue => error
+      erb :error, {:layout => false}, :error => error.inspect
     end
+  end
+
+  def strip_redundants(item)
+    item.reject! {|k, v| %w"id name link website cover is_published".include? k }
+    item.keys.each do |key|
+      new_key = key.gsub(/^([a-z])/) { $1.capitalize }
+      new_key = key.gsub('_', ' ')
+      item[new_key] = item[key]
+      item.delete(key)
+    end
+
+    item
+  end
+
+  def get_links(item)
+    links = {}
+    links["name"] = item["name"] if item.has_key?("name")
+    links["fb_page"] = item["link"] if item.has_key?("link")
+    links["website"] = item["link"] if item.has_key?("website")
+    links["avatar"] = item["cover"]["source"] if item.has_key?("cover")
+
+    links
   end
 end
